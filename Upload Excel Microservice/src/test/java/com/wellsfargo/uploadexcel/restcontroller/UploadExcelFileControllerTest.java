af81c2adf6b86d2838d579144f026cc0468c1b58
tantrojan <tg.99ghosh@gmail.com>
2020-09-01 20:28:08 +0530

    Add Upload Excel Microservice Diagrams

---
+++ Upload Excel Microservice/src/test/java/com/wellsfargo/uploadexcel/restcontroller/UploadExcelFileControllerTest.java
@@ -0,0 +1,32 @@
+package com.wellsfargo.uploadexcel.restcontroller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.junit.Before;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.wellsfargo.uploadexcel.service.UploadExcelService;
+import com.wellsfargo.uploadexcel.service.UploadExcelServiceImpl;
+
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+class UploadExcelFileControllerTest {
+	
+}
